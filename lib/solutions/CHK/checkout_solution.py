67245b7ee1d6637192dd8e0c45ec7efeb3143365
sbx_user1051 <sbx_user1051@169.254.13.117>
2021-11-07 14:13:02 +0000

    Sun Nov 07 14:13:02 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,11 +5,17 @@
 def checkout(skus):  
     
     class SKU_database:
-        def __init__(self, cost, offer_available, offer_quantity, offer_cost):
+        def __init__(self, cost, offer_available, offer_quantity, offer_cost, 
+                     multi_offer_available, multi_offer_quantity, multi_offer_free):
             self.cost = cost
             self.offer_available = offer_available
             self.offer_quantity = offer_quantity
             self.offer_cost = offer_cost
+            self.multi_offer_available = multi_offer_available
+            self.multi_offer_quantity = multi_offer_quantity
+            self.multi_offer_free = multi_offer_free
+            
+            
             
         def get_total_price(self, quantity):
             
@@ -25,6 +31,20 @@ def checkout(skus):
             else:
                 return quantity * self.cost
             
+            
+            
+        def get_multiple_item_promotion(self, quantity):
+            
+            if(self.multi_offer_available):
+                #All possible free items available with offer
+                total_free = quantity // self.multi_offer_quantity           
+                return multi_offer_free * total_free
+            
+            else :
+                return ""
+            
+            
+            
     
     sku_database = {"A" : SKU_database(50, True, 3, 130),
                     "B" : SKU_database(30, True, 2, 45),
@@ -36,6 +56,9 @@ def checkout(skus):
     total = 0
     #Get all unique items in the list
     unique_items = set(skus)
+    
+    #Check for offers involving multiple items first, as this might modify quantities
+   # available_free_items = 
     
     
     for item in unique_items:
@@ -51,5 +74,6 @@ def checkout(skus):
             return -1
     
     return total
+
 
 
