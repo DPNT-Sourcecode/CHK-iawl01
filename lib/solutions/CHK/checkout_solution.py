23ca334314f4dbbd2178b0cdfaf996213b3f4802
sbx_user1051 <sbx_user1051@169.254.13.117>
2021-11-07 13:43:31 +0000

    Sun Nov 07 13:43:31 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,10 +3,22 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):  
-    sku_database = {"A" : 50,
-                    "B" : 30,
-                    "C" : 20,
-                    "D" : 15
+    
+    class SKU_database:
+        def __init__(self, cost, offer_available, offer_quantity, offer_price):
+            self.cost = cost
+            self.offer_available = offer_available
+            self.offer_quantity = offer_quantity
+            self.offer_price = offer_price
+            
+        def get_total_price(self, quantity):
+            return 
+            
+    
+    sku_database = {"A" : SKU_database(50, False, 0, 0),
+                    "B" : SKU_database(30, False, 0, 0),
+                    "C" : SKU_database(20, False, 0, 0),
+                    "D" : SKU_database(15, False, 0, 0)
                     }
     
     total = 0
@@ -16,6 +28,7 @@ def checkout(skus):
     
     for item in unique_items:
 
+        #Count number of instances of an item being checked out
         item_quantity = skus.count(item)
         
         try:
@@ -26,6 +39,7 @@ def checkout(skus):
             return -1
     
     return total
+
 
 
 
