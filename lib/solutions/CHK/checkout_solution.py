1967353ed037640d04abb8ab2a2b8d4b0ea5e1e5
sbx_user1051 <sbx_user1051@169.254.13.117>
2021-11-07 13:28:31 +0000

    Sun Nov 07 13:28:31 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,15 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+def checkout(skus):  
+    sku_database = {"A" : 50,
+                    "B" : 30,
+                    "C" : 20,
+                    "D" : 15
+                    }
+    total = 0
+    
+    
+    return total
+
+
